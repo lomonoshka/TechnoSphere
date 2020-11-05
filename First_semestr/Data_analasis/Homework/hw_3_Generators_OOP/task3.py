5a0a6998d198301ee4cb63dd46e862a6407ff223
pigerloafer <kruj1998@gmail.com>
2020-11-06 01:47:32 +0300

    hw2 on ml add

--- First_semestr/Data_analasis/Homework/hw_3_Generators_OOP/task3.py
+++ First_semestr/Data_analasis/Homework/hw_3_Generators_OOP/task3.py
@@ -2,7 +2,6 @@ from time import sleep
 import functools
 import signal
 
-
 class TimeoutException(RuntimeError):
     def __init__(self, message=None):
         super().__init__(message)
@@ -13,17 +12,40 @@ def handler(signum, frame):
 
 
 def timeout(seconds):
-    def decorator(func):
-        if(seconds is None or seconds <= 0):
-            return func
+  def decorator(func):
+      if(seconds is None or seconds <= 0):
+        return func
+      @functools.wraps(func)
+      def wrapper(*args, **argv):
+          signal.signal(signal.SIGALRM, handler)
+          signal.setitimer(signal.ITIMER_REAL, seconds)
+          result = func(*args, **argv)
+          signal.signal(signal.SIG_DFL)
+          return result
+      return wrapper
+  return decorator
 
-        @functools.wraps(func)
-        def wrapper(*args, **argv):
-            signal.signal(signal.SIGALRM, handler)
-            signal.setitimer(signal.ITIMER_REAL, seconds)
-            result = func(*args, **argv)
-            signal.setitimer(signal.ITIMER_REAL, 0)
-            signal.signal(signal.SIGALRM, signal.SIG_DFL)
-            return result
-        return wrapper
-    return decorator
+
+# @timeout(seconds=0.5)
+# def func1():
+#     sleep(0.1)
+
+
+# try:
+#     func1()
+# except TimeoutException as e:
+#     print(e)
+
+
+@timeout(seconds= )
+def func2():
+    sleep(0.3)
+
+def main():
+  try:
+      func2()
+  except TimeoutException as e:
+      print(e)
+
+if __name__ == "__main__":
+  main()
